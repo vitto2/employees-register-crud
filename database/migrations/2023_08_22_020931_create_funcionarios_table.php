f4b2991c088369fecd8034de0c8c3bef00aa1fcd
Vitor <vitor2071@gmail.com>
2023-08-22 09:14:29 -0300

    fix: corrigindo nome da model

--- database/migrations/2023_08_22_020931_create_funcionarios_table.php
+++ database/migrations/2023_08_22_020931_create_funcionarios_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('funcionarios', function (Blueprint $table) {
+        Schema::create('Employee', function (Blueprint $table) {
             $table->id();
             $table->string('nome');
             $table->string('email');
@@ -26,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('funcionarios');
+        Schema::dropIfExists('Employee');
     }
 };
