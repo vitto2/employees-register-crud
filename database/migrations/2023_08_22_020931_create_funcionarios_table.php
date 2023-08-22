9a90e37928f2b18cf53103c0c7f14a3901497c6a
Vitor <vitor2071@gmail.com>
2023-08-21 23:19:30 -0300

    feat: criando tabela principal no bd do projeto

---
+++ database/migrations/2023_08_22_020931_create_funcionarios_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('funcionarios', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome');
+            $table->string('email');
+            $table->string('cpf');
+            $table->date('data_admissao');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('funcionarios');
+    }
+};
