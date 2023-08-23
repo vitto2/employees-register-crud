76d2947d9bb89477d4043c14038e2338807ea5d5
Vitor <vitor2071@gmail.com>
2023-08-23 18:47:01 -0300

    fix: ajustando rotas

--- routes/api.php
+++ routes/api.php
@@ -9,11 +9,8 @@ use Illuminate\Support\Facades\Route;
 Route::post('/register-employee', [EmployeeController::class,'store']);
 
 Route::get('/employees', [EmployeeController::class,'index']);
-
 Route::get("/employee/{id}", [EmployeeController::class,'show']);
-
 Route::put("/employee/{id}",[EmployeeController::class,'update']);
-
 Route::delete("/employee/{id}",[EmployeeController::class,'destroy']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
